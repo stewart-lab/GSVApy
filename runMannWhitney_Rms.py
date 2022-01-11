a93c2bc21e295519c017d7172e3331815af970fb
Ron Stewart <rstewart@morgridge.org>
2022-01-11 16:41:21 -0600

    added ability to do either mann-whitney or wilcoxon signed rank test for paired data

--- runMannWhitney_Rms.py
+++ runMannWhitney_Rms.py
@@ -23,10 +23,17 @@ def main():
     rmslogging.write_args_and_files(my_args, sys.argv[0], COMMAND_LINE_DEF_FILE, logfile)
     
     in_file = my_args["in_file"]
-    out_file = os.path.join(my_args["out_dir"], "output_mw.tsv")
+    paired_data = my_args["paired_data"]
+    
+    if paired_data:
+        out_file_name = "output_wilcoxonpaired.tsv"
+    else:
+        out_file_name = "output_mw.tsv"    
+    out_file = os.path.join(my_args["out_dir"], out_file_name)
     out_wilks = os.path.join(my_args["out_dir"], "output_wilks.tsv")
     cond1_names = my_args["cond1_names"]
     cond2_names = my_args["cond2_names"]
+    
     
     cond1_names = cond1_names.split(",")
     cond2_names = cond2_names.split(",")
@@ -47,10 +54,10 @@ def main():
     #  if most p-values are < 0.05, then the data is not normally distributed, and you should use a
     #  mann-whitney test, and NOT a t-test.
     #df2 = df[cond1_names + cond2_names]
-    mw_stat_dict = OrderedDict()
-    mw_pval_dict = OrderedDict()
-    mw_adj_pval_dict = OrderedDict()
-    mw_pval_list = []
+    stat_dict = OrderedDict()
+    pval_dict = OrderedDict()
+    adj_pval_dict = OrderedDict()
+    pval_list = []
     with open(out_file, 'w') as out_mw:
         if (my_args["show_wilks"]):
             with open(out_wilks, 'w') as out_wilks:
@@ -66,24 +73,28 @@ def main():
                         out_wilks.write(f"YES\t{row[0]}\t{w}\t{pvalue}\n")  
                         normal_count = normal_count + 1  
                 out_wilks.write(f"Normal Count\t{normal_count}\tNOT Normal Count\t{not_normal_count}\n")          
-        out_mw.write(f"Term\tMW Stat\tMW P Value\tMW Adj P Value\n")  
+        out_mw.write(f"Term\tStat\tP Value\tAdj P Value\n")  
         for _, row in df.iterrows():
-            mw = stats.mannwhitneyu(x=row[cond1_names].astype(float), y=row[cond2_names].astype(float), alternative="two-sided")       
+            if paired_data:
+                stat_obj = stats.wilcoxon(x=row[cond1_names].astype(float), y=row[cond2_names].astype(float), alternative="two-sided")       
+           
+            else:
+                stat_obj = stats.mannwhitneyu(x=row[cond1_names].astype(float), y=row[cond2_names].astype(float), alternative="two-sided")       
            
            
-            mw_stat_dict[row[0]] = mw.statistic
-            mw_pval_dict[row[0]] = mw.pvalue
-            mw_pval_list.append(mw.pvalue)
+            stat_dict[row[0]] = stat_obj.statistic
+            pval_dict[row[0]] = stat_obj.pvalue
+            pval_list.append(stat_obj.pvalue)
             #print ("mw.pvalue:", mw.pvalue)
             #out_mw.write(f"{row[0]}\t{mw.statistic}\t{mw.pvalue}\n")
            
-        rej, adj_pvals, _, _ = sm.multipletests(mw_pval_list, alpha=0.05, method='fdr_bh')      
-        for i, term in enumerate(mw_stat_dict.keys()): 
+        rej, adj_pvals, _, _ = sm.multipletests(pval_list, alpha=0.05, method='fdr_bh')      
+        for i, term in enumerate(stat_dict.keys()): 
             #print("i:", i, " adjpval:", adj_pvals[i])
-            mw_adj_pval_dict[term] = adj_pvals[i]
-        for term in mw_stat_dict.keys():
-            if mw_pval_dict[term] < 0.05:
-                out_mw.write(f"{term}\t{mw_stat_dict[term]}\t{mw_pval_dict[term]}\t{mw_adj_pval_dict[term]}\n")
+            adj_pval_dict[term] = adj_pvals[i]
+        for term in stat_dict.keys():
+            if pval_dict[term] < 0.05:
+                out_mw.write(f"{term}\t{stat_dict[term]}\t{pval_dict[term]}\t{adj_pval_dict[term]}\n")
                         
     rmslogging.close_log_file(logfile)  
     (end_time_secs, x) = rmstime.get_time_and_pretty_time()
